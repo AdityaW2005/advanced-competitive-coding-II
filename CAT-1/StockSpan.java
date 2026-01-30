93c032bf5bac17378927b8e2c42e52fed514a23c
AdityaW2005 <adhiw2005@gmail.com>
2026-01-30 12:38:47 +0530

    feat: add StockSpan.java for calculating stock span values

---
+++ CAT-1/StockSpan.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class StockSpan {
+    static void span(int p[], int n, int s[]) {
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        s[0] = 1;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && p[st.peek()] <= p[i]) {
+                st.pop();
+            }
+            s[i] = (st.empty() ? (i + 1) : (i - st.peek()));
+            st.push(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p[] = new int[n];
+        for (int i = 0; i < n; i++)
+            p[i] = sc.nextInt();
+        int s[] = new int[n];
+        span(p, n, s);
+        for (int i = 0; i < n; i++)
+            System.out.print(s[i] + " ");
+        sc.close();
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
