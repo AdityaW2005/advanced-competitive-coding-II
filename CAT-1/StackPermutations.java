0509628c7f21d2057eaf5416f2324ee912526a0e
AdityaW2005 <adhiw2005@gmail.com>
2026-01-30 15:35:33 +0530

    feat: add StackPermutations.java for checking stack permutations of two queues

---
+++ CAT-1/StackPermutations.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class StackPermutations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+
+        for (int i = 0; i < n; i++)
+            q1.add(sc.nextInt());
+
+        for (int i = 0; i < n; i++)
+            q2.add(sc.nextInt());
+
+        Stack<Integer> st = new Stack<>();
+
+        while (!q1.isEmpty()) {
+            int ele = q1.poll();
+
+            if (ele == q2.peek()) {
+                q2.poll();
+
+                while (!st.isEmpty() && st.peek() == q2.peek()) {
+                    st.pop();
+                    q2.poll();
+                }
+            } else {
+                st.push(ele);
+            }
+        }
+
+        if (q1.isEmpty() && st.isEmpty())
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+
+        sc.close();
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
